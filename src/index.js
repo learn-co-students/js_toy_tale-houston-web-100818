ea7d993c126ac885dd24f408582c72a08001c237
Megan Farley <megfarley4@gmail.com>
2018-11-27 15:36:14 -0600

    rendered all toys. next is adding toys

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyCollection = document.querySelector('#toy-collection')
+
+
 let addToy = false
 
-// YOUR CODE HERE
+const render = function(){
+  fetch('http://localhost:3000/toys')
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(toys){
+      console.log(toys)
+      renderToyCollection(toys)
+    })
+}
+
+const renderToyCollection = function(toys){
+  toyCollection.innerHTML = ''
+  toys.forEach(function(toy){
+    const addToyCard = document.createElement('div')
+    addToyCard.className = "card"
+
+    const addToyName = document.createElement('h1')
+    addToyName.innerHTML = toy.name
+
+    const addToyImage = document.createElement('img')
+    addToyImage.className = "toy-avatar"
+    addToyImage.src = toy.image
+
+    const addToyLikes = document.createElement('p')
+    addToyLikes.innerHTML = `${toy.likes} Likes`
+
+    const addToyLikeButton = document.createElement('button')
+    addToyLikeButton.className = "like-btn"
+    addToyLikeButton.innerHTML = "Like <3"
+
+    toyCollection.append(addToyCard)
+    addToyCard.append(addToyName)
+    addToyCard.append(addToyImage)
+    addToyCard.append(addToyLikes)
+    addToyCard.append(addToyLikeButton)
+  })
+}
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -15,5 +55,6 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+render()
 
 // OR HERE!
