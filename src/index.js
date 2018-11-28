57435f4df88179851a3cd75800690e5ed26ced3a
github username <github email address>
2018-11-27 23:42:00 -0600

    Update, Create, Fetch and render

--- src/index.js
+++ src/index.js
@@ -1,19 +1,108 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+const addBtn = document.querySelector("#new-toy-btn");
+const toyForm = document.querySelector(".container");
+const toyCollection = document.querySelector("#toy-collection");
+const formName = document.querySelector(".name-field");
+const formImage = document.querySelector('[type="Image"]');
+const formSubmit = document.querySelector('[type="submit"]');
+let addToy = false;
 
-// YOUR CODE HERE
-
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
   // hide & seek with the form
-  addToy = !addToy
+  addToy = !addToy;
   if (addToy) {
-    toyForm.style.display = 'block'
+    toyForm.style.display = "block";
     // submit listener here
   } else {
-    toyForm.style.display = 'none'
+    toyForm.style.display = "none";
   }
-})
-
+});
 
 // OR HERE!
+
+// This is a function that fetches our data -(render) function
+const fetchData = function() {
+  fetch("http://localhost:3000/toys")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(toys) {
+      renderToys(toys);
+    });
+};
+
+// This will render the toys we get from the Json data
+const renderToys = function(toys) {
+  // This takes the data from toys, makes it so we can access each one individually (in this instance, it's in the word "toy" and then have access to it's unique attributes)
+  toys.forEach(function(toy) {
+    const toyName = document.createElement("h2");
+    const toyImage = document.createElement("img");
+    const toyCard = document.createElement("div");
+    const toyLikes = document.createElement("p");
+    const likeButton = document.createElement("button");
+
+    //Set toy card with a class name of card, for css purposes
+    toyCard.className = "card";
+    //SEt toy name to an HTML element
+    toyName.innerHTML = toy.name;
+    //Set Image to an html element with the class name of toy-avatar
+    toyImage.className = "toy-avatar";
+    toyImage.src = toy.image;
+
+    toyLikes.innerHTML = `${toy.likes} likes`;
+
+    likeButton.innerHTML = "Like";
+    likeButton.className = "like-btn";
+
+    toyCard.append(toyName);
+    toyCard.append(toyImage);
+    toyCard.append(toyLikes);
+    toyCard.append(likeButton);
+    toyCollection.append(toyCard);
+
+    // Create your event listener for a button
+
+    likeButton.addEventListener("click", function(event) {
+      likeToy(toy);
+    });
+  });
+};
+
+// this is a function for our event listeners!  So that we can add new toys!
+
+// CODE HERE
+
+const createToy = function() {
+  // this fetches but does a POST request instead of a GET request
+
+  fetch(`http://localhost:3000/toys/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: ".name-field".value,
+      image: ".image-field".value,
+      likes: 0
+    })
+  }).then(fetchData);
+};
+
+// This is a function that updates our data
+const likeToy = function(toy) {
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      likes: toy.likes + 1
+    })
+  }).then(fetchData); //or (render)
+};
+
+submitToy.addEventListener('click', (e) => {
+  e.preventDefault();
+
+// This invokes our fetch function
+fetchData();
